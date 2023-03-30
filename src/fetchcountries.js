5c0810e83851c60ea359796f49a200c9e0d4dfd4
Zerossik <zeross19940303@gmail.com>
2023-03-31 01:10:53 +0300

    module-10 done

--- src/fetchcountries.js
+++ src/fetchcountries.js
@@ -1,7 +1,5 @@
 export function getDate(name) {
-  return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=flags,name`
-  ).then(response => {
+  return fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
--- src/index.js
+++ src/index.js
@@ -3,45 +3,72 @@ import Debounce from 'lodash.debounce';
 import { getDate } from './fetchcountries';
 import { Notify } from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 1000;
 const inputEl = document.querySelector('#search-box');
 const listEl = document.querySelector('.country-list');
-const countriesInfoEl = document.querySelector('.country-info')
+const countriesInfoEl = document.querySelector('.country-info');
 
 function hendlerInputValue() {
   if (inputEl.value === '') {
-    listEl.innerHTML = ''
+    listEl.innerHTML = '';
+    countriesInfoEl.innerHTML = '';
     return;
   }
 
   getDate(inputEl.value.trim()).then(data => {
-    
     if (data.length > 10) {
       Notify.info('Too many matches found. Please enter a more specific name.');
+      listEl.innerHTML = '';
+      countriesInfoEl.innerHTML = '';
       return;
     } else if (data.length >= 2) {
+      countriesInfoEl.innerHTML = '';
       const countries = data
-        .map((el)=> {
+        .map(el => {
           return `
           <li class="country-list__item">
               <img src="${el.flags.svg}" width="30" alt="${el.flags.alt}">
               <p>${el.name.common}</p>
           </li>
-              `
+              `;
         })
         .join(' ');
       listEl.insertAdjacentHTML('beforeend', countries);
-    }else {
+    } else if (data.length === 1) {
       listEl.innerHTML = '';
-      console.log('else');
+      markupCountry(data);
+      return;
     }
-    
   });
+}
+function markupCountry(date) {
+  const country = date.map(el => {
+    console.log(el);
+    return `
+    <div class="contry-wrap">
+    <img src="${el.flags.svg}" alt="${el.flags.alt}" width="100">
+    <h1>${el.name.official}</h1>
+    </div>
+    <div class="country-desc">
+    <p class ="country-text">Capital: <span class="country-value">${
+      el.capital
+    }</span></p>
+    <p class ="country-text">Population: <span class="country-value">${
+      el.population
+    }</span></p>
+    <p class ="country-text">Languages: <span class="country-value">${Object.values(
+      el.languages
+    )}</span></p>
+    </div>
+    `;
+  });
+  countriesInfoEl.innerHTML = '';
+
+  countriesInfoEl.insertAdjacentHTML('beforeend', country);
 }
 
 // Получить доступ к инпуту, и повесить на него событие input/ Событие должно срабатывать не чаще 300мс.
-//(для этого используем библиотеку(lodash.debounce.)).
-// Значение с инпута сохранить в переменную. Удалить все пробелы с помощью метода tream()
+// Значение с инпута сохранить в переменную. Удалить все пробелы с помощью метода tream()   languages
 // написать функцию, которая будет делать запрос на сервер и возвращать результат.
 
 // Сделать валидацию ответа от сервера, если длина массива больше 10, то вывести Алерт ("Too many matches found. Please enter a more specific name.")
