fe72e240d85b4f5cb29f19a70dcea5522fb2dad0
Zerossik <zeross19940303@gmail.com>
2023-03-30 20:48:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './css/styles.css';
 import Debounce from 'lodash.debounce';
 import { getDate } from './fetchcountries';
-// import { Report } from 'notiflix/build/notiflix-report-aio';
 import { Notify } from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
